1c9eba031443c7bd50a97d77e1ae0549bde111a9
Harfinnn <harfinaqbil07@gmail.com>
2025-10-25 10:31:36 +0700

    Get, Post, Put student to course

--- lms-be/src/routes/courseRoutes.js
+++ lms-be/src/routes/courseRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { deleteContentCourse, deleteCourse, getCategories, getCourses, getCoursesById, getDetailContent, postContentCourse, postCourse, updateContentCourse, updateCourse } from "../controllers/courseController.js"
+import { deleteContentCourse, deleteCourse, deleteStudentToCourse, getCategories, getCourses, getCoursesById, getDetailContent, getStudentsByCourseId, postContentCourse, postCourse, postStudentToCourse, updateContentCourse, updateCourse } from "../controllers/courseController.js"
 import { verifyToken } from "../middlewares/verifyToken.js"
 import multer from "multer"
 import { fileFilter, fileStorageCourse } from "../utils/multer.js"
 import { validateRequest } from "../middlewares/validateRequest.js"
-import { mutateContentSchema } from "../utils/schema.js"
+import { addStudentCourseSchema, mutateContentSchema } from "../utils/schema.js"
 
 const courseRoutes = express.Router()
 
@@ -24,5 +24,9 @@ courseRoutes.post('/courses/contents', verifyToken, validateRequest(mutateConten
 courseRoutes.put('/courses/contents/:id', verifyToken, validateRequest(mutateContentSchema), updateContentCourse)
 courseRoutes.delete('/courses/contents/:id', verifyToken, deleteContentCourse)
 courseRoutes.get('/courses/contents/:id', verifyToken, getDetailContent)
+
+courseRoutes.get('/courses/students/:id', verifyToken, getStudentsByCourseId)
+courseRoutes.post('/courses/students/:id', verifyToken, validateRequest(addStudentCourseSchema), postStudentToCourse)
+courseRoutes.put('/courses/students/:id', verifyToken, validateRequest(addStudentCourseSchema), deleteStudentToCourse)
 
 export default courseRoutes
